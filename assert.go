4d6d5ce4d13d03127f42832e2ccf7b15f3844681
bgaudino <bgaudino@gmail.com>
2023-06-12 21:56:30 -0500

    Start asserts

---
+++ assert.go
@@ -0,0 +1,50 @@
+package helpers
+
+import (
+	"errors"
+	"fmt"
+	"strings"
+
+	"golang.org/x/exp/constraints"
+)
+
+var ErrAssertion error = errors.New("assertion failed")
+
+func Assert(condition bool, messages ...string) {
+	message := strings.Join(messages, "\n")
+	err := errors.New(message)
+	if len(message) == 0 {
+		err = ErrAssertion
+	}
+	if !condition {
+		panic(err)
+	}
+}
+
+func AssertEqual[T comparable](x, y T) {
+	Assert(x == y, fmt.Sprintf("%v does not equal %v", x, y))
+}
+
+func AssertTrue(condition bool) {
+	Assert(condition)
+}
+
+func AssertFalse(condition bool) {
+	Assert(!condition)
+}
+
+func AssertGreaterThan[T constraints.Ordered](x, y T) {
+	Assert(x > y, fmt.Sprintf("%v is not greater than %v", x, y))
+}
+
+func AssertGreaterThanOrEqual[T constraints.Ordered](x, y T) {
+	Assert(x >= y, fmt.Sprintf("%v is not greater than or equal to %v", x, y))
+}
+
+func AssertLessThan[T constraints.Ordered](x, y T) {
+	Assert(x < y, fmt.Sprintf("%v is not less than %v", x, y))
+}
+
+func AssertLessThanOrEqual[T constraints.Ordered](x, y T) {
+	Assert(x <= y, fmt.Sprintf("%v is not less than or equal to %v", x, y))
+}
---
+++ assert_test.go
@@ -0,0 +1,28 @@
+package helpers
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestAssert(t *testing.T) {
+	defer func() {
+		if r := recover(); r != nil {
+			err, ok := r.(error)
+			assert.True(t, ok, "Assert() failed to panic with an error")
+			assert.Error(t, err)
+		} else {
+			t.Errorf("Assert() did not panic as expected")
+		}
+	}()
+
+	Assert(false)
+	AssertEqual(1, 2)
+	AssertTrue(false)
+	AssertFalse(true)
+	AssertGreaterThan(1, 2)
+	AssertGreaterThanOrEqual(1, 2)
+	AssertLessThan(2, 1)
+	AssertLessThanOrEqual(2, 1)
+}
